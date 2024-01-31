c81a3aabd33ab3e0bf73e9bfe176c04e6ce1cd74
doogie05 <82701950+doogie05@users.noreply.github.com>
2024-01-31 11:28:38 -0800

    Update deliver.sh
    add workaround for maven NAME and VERSION queries

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -12,15 +12,17 @@ echo 'The following command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 set -x
 NAME=`mvn -q -DforceStdout help:evaluate -Dexpression=project.name`
+NAME2=my-app
 set +x
 
 echo 'The following command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
 VERSION=`mvn -q -DforceStdout help:evaluate -Dexpression=project.version`
+VERSION2=1.0-SNAPSHOT
 set +x
 
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-java -jar target/${NAME}-${VERSION}.jar
+java -jar target/${NAME2}-${VERSION2}.jar
